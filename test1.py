dbe6b6b6eebcc271827d1ebae56932815839a6c9
albastienex <a980364@gmail.com>
2021-01-25 15:45:56 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,14 @@
+print('Hello, World!')
+num1=int(input('number 1:'))
+num2=int(input('number 2:'))
+num3=int(input('number 3:'))
+print("the biggest number is:", max(num1, num2, num3))
+
+account=str(input('type your account:'))
+password=str(input('type your password'))
+success=(account=="1234" and password=='4567')
+print('登入成功', success)
+
+a=int(input('a='))
+b=int(input('b='))
+print(a+b)
