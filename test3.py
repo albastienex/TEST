15cf7fefca4eb1322ad9658a3f546ca475c7e03b
albastienex <a980364@gmail.com>
2021-01-25 16:46:08 +0800

    1-7

---
+++ test3.py
@@ -0,0 +1,14 @@
+h1=int(input())
+m1=int(input())
+s1=int(input())
+h2=int(input())
+m2=int(input())
+s2=int(input())
+print((h2*60*60+m2*60+s2)-(h1*60*60+m1*60+s1))
+
+# Given two timestamps of the same day: 
+# a number of hours, minutes and 
+# seconds for both of the timestamps. 
+# The moment of the first timestamp 
+# happened before the moment of the second one.
+# Calculate how many seconds passed between them.
