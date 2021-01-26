8051989c2f1c3b028507784a8b2b718282b4cd87
John Doe <a980364@gmail.com>
2021-01-26 14:48:49 +0800

    4.6

--- test4.py
+++ test4.py
@@ -1,2 +1,13 @@
+sum=1
 n=int(input())
-print(str(n))
+for i in range(1,n+1):
+    sum*=i 
+print(sum)
+
+
+
+#In mathematics, the factorial of an integer n, denoted by n! is the following product:
+
+# n! = 1 × 2 × … × n
+
+# For the given integer n calculate the value n!. Don't use math module in this exercise.
