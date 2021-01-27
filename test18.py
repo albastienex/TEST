61de067c3436171d2c49d9e979af911daa3fbe06
albastienex <a980364@gmail.com>
2021-01-27 10:47:32 +0800

    6-5

---
+++ test18.py
@@ -0,0 +1,16 @@
+a=int(input())
+i=0
+while a !=0:
+    i+=1
+    a=int(input())
+print(i)
+
+# Given a sequence of non-negative 
+# integers, where each number is written
+# in a separate line.
+# Determine the length of the sequence.
+# The sequence ends with 0.
+# Print the length of the sequence 
+# (not counting the 0). 
+# The numbers following the number 0 
+# should be omitted.
