451fcf8e298e845f5b29b92c93c4f3c43ea7e5a7
albastienex <a980364@gmail.com>
2021-01-27 13:43:17 +0800

    for

---
+++ test24.py
@@ -0,0 +1,11 @@
+s=[0,1,2,3]
+for i in s:
+    print(i, end=' ')
+
+for i in range(10):
+    print(i, end=' ')
+# 從0走到10但不包含10
+for i in range(10):
+    i=i+1         
+    print(i, end=' ')    
+#從0走到9再加上1(9+1) 
