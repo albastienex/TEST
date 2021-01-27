5d942483ca86faee9ad3d8f4b3c06a6bdc505f19
albastienex <a980364@gmail.com>
2021-01-27 10:52:41 +0800

    6-6

---
+++ test19.py
@@ -0,0 +1,14 @@
+a=int(input())
+i=0
+sum=0
+while a !=0:
+    i+=1
+    sum +=a
+    a=int(input())
+print(i, sum)
+
+# Given a sequence of non-negative
+# integers, where each number is written
+# in a separate line.
+# The sequence ends with 0.
+# Print the sum of the sequence.
