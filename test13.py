8a17a6d1685d68abbfea61e5a2fe4b3aeb6edf55
albastienex <a980364@gmail.com>
2021-01-27 09:53:41 +0800

    if p

---
+++ test13.py
@@ -0,0 +1,13 @@
+a=2
+b=4
+if a>b:
+    print('a')
+else: 
+    print('b')   
+
+i=1 
+while i<=10:
+    print(i**2)
+    i+=1
+
+
---
+++ test14.py
@@ -0,0 +1,12 @@
+n=int(input())
+i=1
+while i*i<=n:
+    print(i*i,end=' ')
+    i+=1
+
+
+# For a given integer N,
+# print all the squares of positive 
+# integers where the square is less 
+# than or equal to N, in ascending 
+# order.
