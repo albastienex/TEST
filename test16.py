b7fb7d48c4430c72451ab096554ef9ea299473fe
albastienex <a980364@gmail.com>
2021-01-27 10:27:47 +0800

    6-3

---
+++ test16.py
@@ -0,0 +1,13 @@
+x=int(input())
+i= 0
+while 2**(i+1)<=x:
+    i+=1
+print(i, 2**i)
+
+# For a given integer X,
+# find the greatest integer
+# n where 2ⁿ is less than 
+# or equal to X. 
+# Print the exponent value
+# and the result of the
+# expression 2ⁿ.
