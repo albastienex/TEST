8608de1c2d4d738cd33077470fee4e8fba2c04b1
albastienex <a980364@gmail.com>
2021-01-27 12:08:48 +0800

    7-3

---
+++ test23.py
@@ -0,0 +1,10 @@
+a=[int(i) for i in input().split()]
+for i in range(1,len(a)):
+    if a[i-1] < a[i]:
+        print(a[i],end=' ') 
+
+
+# Given a list of numbers, 
+# find and print all its 
+# elements that are greater 
+# than their left neighbor.
