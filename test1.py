1b7097446dd6b5c9e3123e3606175756b116362b
albastienex <a980364@gmail.com>
2021-01-28 09:22:12 +0800

    -

--- test1.py
+++ test1.py
@@ -12,3 +12,8 @@ print('登入成功', success)
 a=int(input('a='))
 b=int(input('b='))
 print(a-b)
+
+age=int(input("年齡:"))
+heart_rate=(220-age)*0.7
+print("有效運動心跳率為:"+str(heart_rate))
+print('Have a nice day!')
