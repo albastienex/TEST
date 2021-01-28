a00eee23a5c6afd6d279df52511199353fddb49e
albastienex <a980364@gmail.com>
2021-01-28 09:25:50 +0800

    -

--- test20.py
+++ test20.py
@@ -2,6 +2,8 @@ a=[1,2,3]
 for item in a:
     print(item, end=' ')
 
-print(*a)
 #兩種語法一樣
+
+print(*a)
+
 
--- 基礎練習.py
+++ 基礎練習.py
@@ -17,3 +17,8 @@ age=int(input("年齡:"))
 heart_rate=(220-age)*0.7
 print("有效運動心跳率為:"+str(heart_rate))
 print('Have a nice day!')
+
+n=input('say sorry')
+while'sorry' not in n :
+    n=input("say sorry")
+print("It's OK")     
