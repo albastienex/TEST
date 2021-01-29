4bf3675874b911aafd8128ed195e6aa26a037691
albastienex <a980364@gmail.com>
2021-01-29 14:11:27 +0800

    -

---
+++ 基礎練習03.py
@@ -0,0 +1,21 @@
+a ={
+    "name":"alex",
+    "nickname":"al",
+    "nationality":"tw",
+}
+
+b={
+    "phone number":"0971344051",
+    "region":"tainan",
+    "membership":"yes",
+}
+
+print("Your phone number is %s"%(b["phone number"]))
+print("You live in %s" %(b["region"]))
+print("membership %s" %(b["membership"]))
+
+print("My name is %s"%(a["name"]))
+print("You can also call me %s" %(a["nickname"]))
+print("I come from %s" %(a["nationality"]))
+
+
